f473ffb3171e6a2f98c8110c34c0fb418becaae6
jobzsu <jbbarredo@gmail.com>
2025-04-13 21:31:04 +0800

    Department List with create and edit

--- SHS.StudentPortal/SHS.StudentPortal.Common/Models/CreateDepartmentViewModel.cs
+++ SHS.StudentPortal/SHS.StudentPortal.Common/Models/CreateDepartmentViewModel.cs
@@ -5,4 +5,13 @@ public class CreateDepartmentViewModel
     public string Name { get; set; }
 
     public string Description { get; set; }
+
+    public static CreateDepartmentViewModel New()
+    {
+        return new CreateDepartmentViewModel
+        {
+            Name = string.Empty,
+            Description = string.Empty
+        };
+    }
 }
--- SHS.StudentPortal/SHS.StudentPortal.Common/Models/DepartmentViewModel.cs
+++ SHS.StudentPortal/SHS.StudentPortal.Common/Models/DepartmentViewModel.cs
@@ -9,4 +9,15 @@ public class DepartmentViewModel
     public string? Description { get; set; }
 
     public List<string> Instructors { get; set; }
+
+    public static DepartmentViewModel New()
+    {
+        return new DepartmentViewModel
+        {
+            Id = 0,
+            Name = string.Empty,
+            Description = string.Empty,
+            Instructors = new List<string>()
+        };
+    }
 }
